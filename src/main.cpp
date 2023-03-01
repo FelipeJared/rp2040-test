720a964da81ef6e7f0a947683eac02d12edb038b
Jared Guerrero <felipejaredgm@gmail.com>
2023-03-01 04:23:12 -0800

    Subscriber listening to /joint1 topic added

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,17 @@
 #include <rclc/executor.h>
 
 #include <std_msgs/msg/int32.h>
+#include <std_msgs/msg/float64.h>
 
 rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
+std_msgs__msg__Float64 msg;
+rcl_subscription_t subscriber;
+std_msgs__msg__Float64 servo1msg;
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
+rcl_node_t nodeSub;
 rcl_timer_t timer;
 
 #define LED_PIN 13
@@ -21,6 +25,16 @@ rcl_timer_t timer;
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
+float servoPosition = 0.0;
+std_msgs__msg__Float64 servoPos;
+
+void subscription_callback(const void * msgin)
+{
+	const std_msgs__msg__Float64 * servo1msg = (const std_msgs__msg__Float64 *)msgin;
+  servoPosition = float(servo1msg->data);
+  msg.data = servoPosition;
+	// printf("Received: %d\n", servo1msg->data);
+}
 
 void error_loop(){
   while(1){
@@ -35,7 +49,7 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
   if (timer != NULL) {
     RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
     //msg.data++;
-    msg.data = digitalRead(12);
+    //msg.data = digitalRead(12);
   }
 }
 
@@ -53,15 +67,21 @@ void setup() {
   //create init_options
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-  // create node
+  // create nodes
   RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
 
   // create publisher
   RCCHECK(rclc_publisher_init_default(
     &publisher,
     &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float64),
     "rp2040_pub"));
+
+  RCCHECK(rclc_subscription_init_default(
+    &subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float64),
+    "joint1"));
 
   // create timer,
   const unsigned int timer_timeout = 1000;
@@ -69,13 +89,17 @@ void setup() {
     &timer,
     &support,
     RCL_MS_TO_NS(timer_timeout),
-    timer_callback));
+    timer_callback));  
 
   // create executor
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  // rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+  RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
   RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &servo1msg, &subscription_callback, ON_NEW_DATA));
 
-  msg.data = 0;
+  msg.data = 0.0;
+  // servo1msg.data = 0.0;
+  // servoPos.data = 0.0;
 }
 
 void loop() {
